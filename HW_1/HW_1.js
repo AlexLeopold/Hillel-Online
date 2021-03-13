8e551a4eecc5dcda0b05237106dc491211642118
AlexLeopold <71559312+AlexLeopold@users.noreply.github.com>
2021-03-13 17:24:39 -0500

    Delete HW_1.js

--- HW_1/HW_1.js
+++
@@ -1,4 +0,0 @@
-let lastName = prompt('Enter your last name');
-let firstName = prompt('Your last name is ' + lastName + ', and what is your first name?');
-let fullName = `${lastName} ${firstName}`;
-alert(`Your full name is ${fullName}!`);
