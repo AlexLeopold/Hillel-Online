8c86cc42259953d3d61db6422583da23d205cd84
AlexLeopold <71559312+AlexLeopold@users.noreply.github.com>
2021-03-13 17:26:44 -0500

    Add files via upload

---
+++ HW_1/HW_1.js
@@ -0,0 +1,4 @@
+let lastName = prompt('Enter your last name');
+let firstName = prompt('Your last name is ' + lastName + ', and what is your first name?');
+let fullName = `${lastName} ${firstName}`;
+alert(`Your full name is ${fullName}!`);
